9a292ce24c46887e346701e8f68fc65904334da3
Baldesito <112170802+Baldesito@users.noreply.github.com>
2024-10-07 15:51:39 +0200

    Commet

---
+++ Risposte.js
@@ -0,0 +1,47 @@
+// Risposta 1.
+
+// Stringhe (string): Sono come le parole anche se sembra un numero, è una stringa perché è tra virgolette ""
+// Numeri (Number): Sono i numeri che usi per contare.
+// Bolleani (boolean): Sono come intoruptori , o delle domande vero o falso.
+// Non Definita (undefined): E come una scatola vuota
+// Nulla (null): E come per dire di base la scattola è vuota.
+// Simboli (Symbol): Sono come etichette che usi per designare delle scatole speciale.
+
+// Risposta 2.
+
+let myName = "Balde";
+console.log(myName);
+
+// Risposta 3.
+
+let resultato = 12 + 20;
+console.log(resultato);
+
+// Risposta 4.
+
+let nameX = 12;
+console.log(nameX);
+
+// Risposta 5.
+
+/* 
+let myName = "Abdoulaye";
+console.log(const);
+*/
+
+// Risposta 6.
+
+let x = 12;
+let result = 4 - x;
+console.log(result);
+
+// Risposta 7.
+
+let name1 = "john";
+let name2 = "John";
+let areDifferent = name1 !== name2;
+console.log(areDifferent);
+let areEqualLowercase = name1.toLowerCase() === name2.toLowerCase();
+console.log(areEqualLowercase);
+
+
